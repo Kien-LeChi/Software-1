8c79d7e04e29eabb589cf8892ac4fb3eebda7086
Kien-LeChi <chikienl@metropolia.fi>
2024-08-24 11:54:33 +0300

    Update Module 2_3 - Rectangle P and S.py

--- Assignments/Module 2/Module 2_3 - Rectangle P and S.py
+++ Assignments/Module 2/Module 2_3 - Rectangle P and S.py
@@ -4,6 +4,6 @@ length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
 
 print(
-    f"The perimeter of the rectangle is {2 * (length * width):.5f}\n"
+    f"The perimeter of the rectangle is {2 * (length + width):.5f}\n"
     f"The area of the rectangle is {width * length:.5f}"
     )
