019160dfd52230555086cc84128d07581fa5e30c
acidentally <chikienl@metropolia.fi>
2024-10-21 10:06:13 +0300

    Update

--- Assignments/Module 5/Module 5_1 - Dice rolling.py
+++ Assignments/Module 5/Module 5_1 - Dice rolling.py
@@ -8,6 +8,6 @@ SumOfDice = 0;
 
 for i in range(TotalDice) :
     dice = rand.randint(1, 6)
+    ## Testing line
     SumOfDice += dice
-
 print("The sum of all dice results is {sum}".format(sum=SumOfDice))
